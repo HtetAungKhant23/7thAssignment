92973766dd5a8d7c65c18a77cb785b564d6cb3f2
Htet Aung Khant <112607740+HtetAungKhant23@users.noreply.github.com>
2022-11-18 09:08:12 +0630

    Create cycleTicketForNCCmain.cpp
    7thAssignment

---
+++ cycleTicketForNCCmain.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Htet Aung Khant on 11/10/2022.
+//
+#include "iostream"
+#include "cycleTicketForNCC.h"
+using namespace std;
+using namespace CYCLE;
+int main(){
+    CycleTicketForNCC obj;
+    obj.welcome();
+    return 0;
+}
